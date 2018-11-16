407c9ce7a670d5fba38b06274da4eacd2f373714
Grzegorz <34044369+grypesc@users.noreply.github.com>
2018-11-16 12:11:59 +0900

    created attendance system

--- client.c
+++ client.c
@@ -3,6 +3,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
+
+#define MAXNAME 30
+
+
+int checkName(char data[])
+{
+  char c = data[0];
+  int i=0;
+  while (c != ' ' && i<MAXNAME)
+  {
+    if (c == '\n') return 1;
+    i++;
+    c=data[i];
+  }
+  c=data[i];
+  int j=0;
+  while (c != '\0'  && j<MAXNAME)
+  {
+    i++;
+    j++;
+    c=data[i];
+  }
+  return 0;
+}
 
 
 int main()
@@ -12,7 +37,8 @@ int main()
   int socketFileDescriptor; //This is the socket file descriptor that will be used to identify the socket
   int conn; //This is the connection file descriptor that will be used to distinguish client connections.
   char message[100] = ""; //This array will store the messages that are sent by the serverer
-
+  memset(message, '\0', 100);
+	char server_reply[2000];
   socketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
 
   server.sin_family = AF_INET;
@@ -20,13 +46,24 @@ int main()
 //
   inet_pton(AF_INET, "127.0.0.1", &server.sin_addr); //This binds the client to localhost
 
-  connect(socketFileDescriptor, (struct sockaddr *)&server, sizeof(server)); //This connects the client to the serverer.
-
-  while(1) {
-      printf("Enter a message: ");
-      fgets(message, 100, stdin);
-      send(socketFileDescriptor, message, strlen(message), 0);
-      //An extra breaking condition can be added here (to terminate the while loop)
+  if (connect(socketFileDescriptor, (struct sockaddr *)&server, sizeof(server))!=0) { //This connects the client to the serverer.
+    puts("Failed to connect to server");
+    exit(0);
   }
+
+      printf("Enter your name and a surname: ");
+      fgets(message, 100, stdin);
+      while (checkName(message) != 0 )
+        puts ("Enter your name, space and then surname");
+
+      send(socketFileDescriptor, message, strlen(message), 0);
+      if( recv(socketFileDescriptor , server_reply , 2000 , 0) < 0)
+      {
+        puts("Your attendance check failed. Please try again or contact professor.");
+        exit(1);
+      }
+      puts(server_reply);
+      //An extra breaking condition can be added here (to terminate the while loop)
+
 
 }
--- server.c
+++ server.c
@@ -3,13 +3,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/mman.h>
+
+#define MAXNAME 30
+
+struct Node {
+  char name[30];
+  char surname[30];
+  struct Node *next;
+};
+
+  struct Node *head = NULL;
+
+
+ void addToList(char data[]) {
+
+  struct Node *new = malloc(sizeof(struct Node));
+
+
+  char c = data[0];
+  int i=0;
+  while (c != ' ' && i<MAXNAME)
+  {
+    new->name[i]=c;
+    i++;
+    c=data[i];
+
+  }
+  new->name[i]='\0';
+  i++;
+  c=data[i];
+  int j=0;
+  while (c != '\0'  && j<MAXNAME)
+  {
+    new->surname[j]=c;
+    i++;
+    j++;
+    c=data[i];
+  }
+  new->surname[j]=c;
+  new->next = head;
+  head = new;
+}
+
+void printList() {
+  struct Node* current = head;
+  int i=1;
+  printf("no. Name                           Surname\n");
+  while(current != NULL) {
+    if(i<10)
+      printf("0");
+    printf("%d. %s",i, current->name);
+    int len = strlen(current->name);
+    for (len;len<=MAXNAME;len++)
+      printf (" ");
+
+    printf("%s", current->surname);
+    current = current->next;
+    i++;
+  }
+  printf("\n");
+}
+
+int saveList() {
+  FILE *fd;
+  fd = fopen("attendanceList.txt", "w");
+  fprintf(fd, "no. Name                           Surname\n");
+  int i=0;
+
+  struct Node *current = head;
+  int len = 0;
+  while(current != NULL) {
+    if(i<10)
+      fprintf(fd, "0");
+    fprintf(fd, "%d. %s",i, current->name);
+    len = strlen(current->name);
+    for (len;len<=MAXNAME;len++)
+      fprintf (fd, " ");
+
+    fprintf(fd, "%s", current->surname);
+    current = current->next;
+    i++;
+  }
+  fclose(fd);
+}
 
 int main()
 {
   struct sockaddr_in server; //This is our main socket variable.
   int socketFileDescriptor; //This is the socket file descriptor that will be used to identify the socket
-  int conn; //This is the connection file descriptor that will be used to distinguish client connections.
+  int client_sock; //This is the connection file descriptor that will be used to distinguish client connections.
   char message[100] = ""; //This array will store the messages that are sent by the serverer
+  char messageToClient[35] = "Your attendance check is completed";
 
   server.sin_family = AF_INET;
   server.sin_port = htons(8096); //Define the port at which the serverer will listen for connections.
@@ -24,16 +113,19 @@ int main()
   listen(socketFileDescriptor,5); //Listen for client connections. Maximum 5 connections will be permitted.
 
   //Now we start handling the connections.
+  struct sockaddr_in clientAddr;
+  socklen_t slen = sizeof(clientAddr);
 
-  while(conn = accept(socketFileDescriptor, (struct sockaddr *)NULL, NULL)) {
-      int pid;
-      if((pid = fork()) == 0) {
-          while (recv(conn, message, 100, 0)>0) {
-              printf("Message Received: %s\n", message);
-              //An extra breaking condition can be added here (to terminate the child process)
-              //message = "";
-          }
-          exit(0);
-      }
+  while(client_sock = accept(socketFileDescriptor, (struct sockaddr *)&clientAddr, &slen )) {
+        memset(message, '\0', 100);
+        recv(client_sock, message, 100, 0);
+        printf("Attendance request received from %s::%d, %s\n", inet_ntoa(clientAddr.sin_addr), (int) ntohs(clientAddr.sin_port), message);
+        addToList(message);
+        write(client_sock , messageToClient , strlen(messageToClient));
+        //An extra breaking condition can be added here (to terminate the child process)
+        //message = "";
+             printList();
+
+             saveList();
   }
 }
